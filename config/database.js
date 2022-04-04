a043755dc83f4a029c70bbc7eb0b7e7134ac80bc
kenlok <ken.lok@eezee.com.sg>
2022-04-04 16:10:54 +0800

    commit 2

--- config/database.js
+++ config/database.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
+// path: ./config/env/production/database.js
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USER'),
+        password: env('DATABASE_PASSWORD'),
     },
-    useNullAsDefault: true,
   },
 });
